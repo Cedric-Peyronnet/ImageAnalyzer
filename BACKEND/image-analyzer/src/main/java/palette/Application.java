bbdec26c01f59d6cb1d8301dbd679210f70f8217
Antonin SPONTAK <antonin.spontak@gmail.com>
2019-01-16 10:09:24 +0100

    refactor: Modified names

--- BACKEND/image-analyzer/src/main/java/palette/Application.java
+++ BACKEND/image-analyzer/src/main/java/palette/Application.java
@@ -3,10 +3,30 @@ package palette;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static palette.util.ColorThief.getPalette;
+
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
+
+
+
+        // JUST FOR TEST
+        try {
+            final BufferedImage testbuf = ImageIO.read(new File("colorpalette.png"));
+            int[][] paletteArrayRgb = getPalette(testbuf, 3);
+            System.out.println(paletteArrayRgb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
         SpringApplication.run(Application.class, args);
     }
 }
--- BACKEND/image-analyzer/src/main/java/palette/PaletteController.java
+++ BACKEND/image-analyzer/src/main/java/palette/PaletteController.java
@@ -7,10 +7,17 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import static palette.util.ColorThief.getPalette;
+
 @RestController
 public class PaletteController {
 
-    @RequestMapping("/palette")
+    @RequestMapping("/randompalette")
     public Palette handleRandomPalette(@RequestParam(value="maincolor", defaultValue="orange") String mainColor) {
         Palette randomPalette = new Palette();
         //juste pour test - il faudrait aussi check notBlank
@@ -28,6 +35,16 @@ public class PaletteController {
 
     @PostMapping("/palette")
     public Palette handleImageUpload(@RequestParam("file") MultipartFile file) {
+
+        try {
+            BufferedImage imgBuff = ImageIO.read(file.getInputStream());
+            int[][] paletteArrayRgb = getPalette(imgBuff, 3);
+            System.out.println(paletteArrayRgb);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
 
         return new Palette(
                 "#457821",
